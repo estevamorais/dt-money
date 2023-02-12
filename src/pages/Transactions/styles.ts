5b1b0c0eb30733fe2a3c05c20773cb271057aa15
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2023-02-12 19:10:23 -0300

    Create SearchForm component

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const TransactionsContainer = styled.main`
   width: 100%;
   max-width: 1120px;
-  margin: 0 auto;
+  margin: 4rem auto 0;
   padding: 0 1.5rem;
 `
 
