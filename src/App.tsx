62933833514ba2d9a4015bab194ccaca892ba249
Estevam Morais <33493699+estevamorais@users.noreply.github.com>
2023-02-12 11:49:39 -0300

    Create Header and Summary components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from 'styled-components'
+import { Transactions } from './pages/Transactions'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>DT Money</h1>
       <GlobalStyles />
+      <Transactions />
     </ThemeProvider>
   )
 }
